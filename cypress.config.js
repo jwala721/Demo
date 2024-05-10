da879fa00c2fa9850bac4e77d839b9b7105d0a77
jwalakr <jwalakr@outlook.com>
2024-05-10 09:40:32 +0530

    push for the 1st time

---
+++ cypress.config.js
@@ -0,0 +1,24 @@
+// const { defineConfig } = require("cypress");
+
+// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// // import allureWriter from "@shelex/cypress-allure-plugin/writer";
+
+// module.exports = defineConfig({
+//     e2e: {
+//         setupNodeEvents(on, config) {
+//             allureWriter(on, config);
+//             return config;
+//         }
+//     }
+// });
+
+const { defineConfig } = require("cypress");
+const { allureCypress } = require("allure-cypress/reporter");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      allureCypress(on);
+    },
+  },
+});
